0e34df12930f6929f533658bfeb579eab9c72ebc
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-03 15:48:49 +0800

    Initial Commit

---
+++ Shared/__swift_emojisApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swift_emojisApp.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct __swift_emojisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
